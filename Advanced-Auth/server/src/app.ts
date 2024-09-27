b592799d62bfc1a43208599ec725bc32dde32f5c
techysiddhant <sidjain8000@gmail.com>
2024-09-27 21:16:26 +0530

    add auth service setup

---
+++ Advanced-Auth/server/src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+import cookieParser from "cookie-parser";
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.get("/", (req, res) => {
+    res.send("Welcome to auth service");
+});
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use(globalErrorHandler);
+export default app;
---
+++ Advanced-Auth/server/src/server.ts
@@ -0,0 +1,18 @@
+import app from "./app";
+import { Config } from "./config";
+import logger from "./config/logger";
+
+const startServer = () => {
+    const PORT = Config.PORT;
+    try {
+        app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
+    } catch (error) {
+        if (error instanceof Error) {
+            logger.error(error.message);
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        }
+    }
+};
+startServer();
