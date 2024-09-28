bebcf55eb4aef13be6dfb598aff1027ebc3c2d8d
techysiddhant <sidjain8000@gmail.com>
2024-09-28 12:37:57 +0530

    fix route

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -4,8 +4,4 @@ const { accessChat, fetchChats } = require("../controllers/chatController");
 const router = express.Router();
 router.route("/").post(protect,accessChat);
 router.route("/").get(protect, fetchChats);
-// router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup);
-// router.route("/groupremove").put(protect, removeFromGroup);
-// router.route("/groupadd").put(protect, addToGroup);
 module.exports = router;
